eb9b273971accdbf5aad83606b78ae44a6e81b4a
Dystian En <distian.enyus@gmail.com>
2022-04-18 20:30:41 +0700

    Add data tables

--- app/Views/layouts/pages/employee/index.php
+++ app/Views/layouts/pages/employee/index.php
@@ -1,62 +1,62 @@
 <?= $this->extend('layouts/app') ?>
+
 <?= $this->section('content') ?>
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <div class="d-sm-flex flex-column mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Employee</h1>
+        <div class="d-sm-flex flex-column mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Employee</h1>
 
-        <nav aria-label="breadcrumb">
-            <ol
-                class="breadcrumb"
-                style="background-color: none"
-            >
-                <li class="breadcrumb-item"><a href="/admin">Home</a></li>
-                <li
-                    class="breadcrumb-item active"
-                    aria-current="page"
-                >Employee</li>
-            </ol>
-        </nav>
-    </div>
-
-    <!-- DataTales Example -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <form class="d-flex justify-content-between">
-                <input
-                    class="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    style="width: 25%"
+            <nav aria-label="breadcrumb">
+                <ol
+                        class="breadcrumb"
+                        style="background-color: none"
                 >
-
-                <a
-                    type="button"
-                    class="btn btn-primary"
-                    href="/admin/employee/add"
-                ><i class="fas fa-fw fa-plus"></i> Create</a>
-            </form>
+                    <li class="breadcrumb-item"><a href="/admin">Home</a></li>
+                    <li
+                            class="breadcrumb-item active"
+                            aria-current="page"
+                    >Employee
+                    </li>
+                </ol>
+            </nav>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table
-                    class="table"
-                    id="dataTable"
-                    width="100%"
-                    cellspacing="0"
-                >
-                    <thead>
+
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <form class="d-flex justify-content-between">
+                    <input
+                            class="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                            style="width: 25%"
+                    >
+
+                    <a
+                            type="button"
+                            class="btn btn-primary"
+                            href="/admin/employee/add"
+                    ><i class="fas fa-fw fa-plus"></i> Create</a>
+                </form>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table
+                            class="display"
+                            id="table_id"
+                    >
+                        <thead>
                         <tr>
-                            <th>No. </th>
+                            <th>No.</th>
                             <th>NIK</th>
                             <th>Fullname</th>
                             <th>Email</th>
                             <th>Phone</th>
                             <th>Aksi</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         <?php $i = 1; ?>
                         <tr>
                             <td>1</td>
@@ -71,63 +71,73 @@
                                 </div>
                             </td>
                         </tr>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Modal Filter -->
-    <div
-        class="modal fade"
-        id="modalFilter"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-        data-backdrop="static"
-    >
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5
-                        class="modal-title"
-                        id="exampleModalLabel"
-                    >Filter</h5>
-                    <button
-                        type="button"
-                        class="close"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                    >
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="jobType">Job Type</label>
-                            <select class="custom-select">
-                                <option selected>Open this select menu</option>
-                                <option value="1">Programmer</option>
-                                <option value="2">UI/UX</option>
-                                <option value="3">Devops</option>
-                            </select>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-dismiss="modal"
-                    >Close</button>
-                    <button
-                        type="button"
-                        class="btn btn-primary"
-                    >Save changes</button>
+        <!-- Modal Filter -->
+        <div
+                class="modal fade"
+                id="modalFilter"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+                data-backdrop="static"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5
+                                class="modal-title"
+                                id="exampleModalLabel"
+                        >Filter</h5>
+                        <button
+                                type="button"
+                                class="close"
+                                data-dismiss="modal"
+                                aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="jobType">Job Type</label>
+                                <select class="custom-select">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">Programmer</option>
+                                    <option value="2">UI/UX</option>
+                                    <option value="3">Devops</option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                        >Close
+                        </button>
+                        <button
+                                type="button"
+                                class="btn btn-primary"
+                        >Save changes
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<?= $this->endSection() ?>
+
+<?= $this->section('scripts') ?>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#table_id').DataTable();
+        });
+    </script>
 <?= $this->endSection() ?>
