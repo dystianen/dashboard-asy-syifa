019b14d6a674d58f953e3547184debff382aa84c
Dystian En <distian.enyus@gmail.com>
2022-06-03 20:09:53 +0700

    chore: size logo absensi

--- app/Views/layouts/pages/login/index.php
+++ app/Views/layouts/pages/login/index.php
@@ -13,44 +13,43 @@
 </head>
 
 <body>
-<div id="auth">
-    <div class="row h-100">
-        <div class="col-lg-5 col-12" style="align-self: center">
-            <div id="auth-left">
-                <h1 class="auth-title"><i class="bi bi-alarm" style="font-size: 60px;"></i> ABSENSI.</h1>
+    <div id="auth">
+        <div class="row h-100">
+            <div class="col-lg-5 col-12" style="align-self: center">
+                <div id="auth-left">
+                    <h1><i class="bi bi-alarm" style="font-size: 40px;"></i> ABSENSI.</h1>
 
-                <?php if (session()->getFlashData('failed')) : ?>
-                    <div class="alert alert-danger" role="alert">
-                        <?php echo session("failed") ?>
-                    </div>
-                <?php endif; ?>
-
-                <p class="auth-subtitle mb-5">Log in with your data that the admin entered.</p>
-
-                <form action="<?php echo base_url(); ?>/login/submit" method="post">
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input name="email" type="text" class="form-control form-control-xl" placeholder="Email">
-                        <div class="form-control-icon">
-                            <i class="bi bi-person"></i>
+                    <?php if (session()->getFlashData('failed')) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo session("failed") ?>
                         </div>
-                    </div>
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input name="password" type="password" class="form-control form-control-xl"
-                               placeholder="Password">
-                        <div class="form-control-icon">
-                            <i class="bi bi-shield-lock"></i>
+                    <?php endif; ?>
+
+                    <p class="auth-subtitle mb-5">Log in with your data that the admin entered.</p>
+
+                    <form action="<?php echo base_url(); ?>/login/submit" method="post">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input name="email" type="text" class="form-control form-control-xl" placeholder="Email">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
                         </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-                    </button>
-                </form>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input name="password" type="password" class="form-control form-control-xl" placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block h-100">
+                <img src="<?= base_url() ?>/assets/images/img-login.jpg" style="width: 100%; height: 100%">
             </div>
         </div>
-        <div class="col-lg-7 d-none d-lg-block h-100">
-            <img src="<?= base_url() ?>/assets/images/img-login.jpg" style="width: 100%; height: 100%">
-        </div>
     </div>
-</div>
 </body>
 
 </html>
