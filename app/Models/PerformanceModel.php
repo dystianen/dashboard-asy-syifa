483988963bd063e8581a809740857bbaf0dd1ce7
Dystian En <distian.enyus@gmail.com>
2022-06-09 12:18:14 +0700

    Chore: id to performanceId

--- app/Models/PerformanceModel.php
+++ app/Models/PerformanceModel.php
@@ -6,16 +6,16 @@ use CodeIgniter\Model;
 
 class PerformanceModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'performances';
-    protected $primaryKey       = 'id';
+    protected $DBGroup = 'default';
+    protected $table = 'performances';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
 
-    protected $allowedFields    = [
+    protected $allowedFields = [
         "user_id",
         "description",
         "score",
@@ -25,25 +25,35 @@ class PerformanceModel extends Model
 
     // Dates
     protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+
+
+    public function getPerformances()
+    {
+        return $this->db
+            ->table('performances')
+            ->join('users', 'users.userId = performances.user_id')
+            ->get()
+            ->getResultArray();
+    }
 }
