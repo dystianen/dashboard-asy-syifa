6dfd3925ff3df56d68211662fe10ea24e891bbe8
Dystian En <distian.enyus@gmail.com>
2022-07-19 12:32:50 +0700

    Fix: point profile

--- app/Views/layouts/pages/User/profile/index.php
+++ app/Views/layouts/pages/User/profile/index.php
@@ -5,7 +5,7 @@
     <div class="row d-flex align-items-center flex-column">
         <img src="https://joeschmoe.io/api/v1/random" class="rounded-circle" alt="..." style="width: 120px; height: 120px">
         <h3 class="text-center mt-4"><?= $user['fullname'] ?></h3>
-        <?php if ($point['total']) : ?>
+        <?php if ($point) : ?>
             <h5 class="text-center"><i class="bi bi-star-fill text-warning"></i> <?= $point['total'] ?></h5>
         <?php else : ?>
             <h5 class="text-center"><i class="bi bi-star-fill text-warning"></i> 0</h5>
